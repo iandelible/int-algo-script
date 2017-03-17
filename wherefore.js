98aabda03625fcc63e2828ee8e223d553252d75b
Ian Pan <ianpan@hotmail.com>
2017-03-17 01:25:10 +0000

    whereore initlal

---
+++ wherefore.js
@@ -0,0 +1,12 @@
+//Make a function that looks thru an arry of objects and returns an array of all object with matching property and value pairs. 
+function whatIsInAName(collection, source) {
+  // What's in a name?
+  var arr = [];
+  // Only change code below this line
+  
+  
+  // Only change code above this line
+  return arr;
+}
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
