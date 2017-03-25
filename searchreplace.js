afad87420d08ea5aaa043226240dce798e9676ae
Ian Pan <ianpan@hotmail.com>
2017-03-25 22:03:09 +0000

    searchreplace add comments

--- searchreplace.js
+++ searchreplace.js
@@ -1,11 +1,14 @@
 function myReplace(str, before, after) {
+  // find index of the first letter of the replacement
   var point = str.indexOf(before);
+  // save the letter to find the casing
   var letter = str[point];
-  var upper = after[0];
   
+  //if letter is lower just use replace(before, after) function
   if (letter === letter.toLowerCase()) {
     return str.replace(before, after);
   }
+  //else make the first letter capital then replace
   else {
     var makeBig = after.charAt(0).toUpperCase() + after.slice(1);
     return str.replace(before, makeBig);
