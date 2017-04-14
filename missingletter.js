274f9689e2dfb053ba8f42eec3c0b9292ef5a7b7
Ian Pan <ianpan@hotmail.com>
2017-04-14 04:02:19 +0000

    missingletter solution

--- missingletter.js
+++ missingletter.js
@@ -1,5 +1,14 @@
 function fearNotLetter(str) {
-  return str;
+  for (i = 0; i < str.length; i++) {
+    //find current charCode
+    var currentCode = str.charCodeAt(i);
+    
+    if (currentCode !== str.charCodeAt(0) + i) {
+      //return the string fromCharCode at currentCode minus 1
+      return String.fromCharCode(currentCode - 1);
+    }
+  }
+  return undefined;
 }
 
 fearNotLetter("abce");
