a4f1d1748e53e0c51651b8dc0e4f2afe567fad73
Ian Pan <ianpan@hotmail.com>
2017-04-15 10:01:21 -0700

    boowho solution

--- boowho.js
+++ boowho.js
@@ -1,7 +1,9 @@
 function booWho(bool) {
   // What is the new fad diet for ghost developers? The Boolean.
-
-  return bool;
+  if (typeof(bool) === 'boolean') {
+    return true;
+  }
+  return false;
 }
 
 booWho(null);
