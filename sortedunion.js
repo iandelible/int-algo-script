1e180136893588ff584f3262c3ab79fa12479139
Ian Pan <ianpan@hotmail.com>
2017-04-17 14:07:28 -0700

    sorted union double for loop solution

--- sortedunion.js
+++ sortedunion.js
@@ -1,5 +1,18 @@
  function uniteUnique(arr) {
-  return arr;
+  //need to read up on arguments object
+  //sorted final array
+  var sortedArray = [];
+
+  for (i = 0; i < arguments.length; i++) {
+    for (j = 0; j < arguments[i].length; j++ ) {
+      if (sortedArray.includes(arguments[i][j])) {}  
+      else {
+        sortedArray.push(arguments[i][j]);
+      }
+    }
+  }
+  
+    return sortedArray;
 }
 
 uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
