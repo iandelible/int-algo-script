340f54f056abbe1c10da4828cc6e39c2343691c0
Ian Pan <ianpan@hotmail.com>
2017-04-20 13:06:22 -0700

    spinaltap first solution

--- spinaltapcase.js
+++ spinaltapcase.js
@@ -1,7 +1,11 @@
 function spinalCase(str) {
   // "It's such a fine line between stupid, and clever."
   // --David St. Hubbins
-  return str;
-}
+  
+  // regex for white space and _
+  var spaceUnderscore = /\s|_/g;
+  // regex for camel cases
+  var camelCase = /([a-z])([A-Z])/g;
+  return str.replace(spaceUnderscore, '-').replace(camelCase, '$1-$2').toLowerCase();
 
 spinalCase('This Is Spinal Tap');
