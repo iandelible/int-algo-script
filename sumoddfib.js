b7e504022ea7f7a13034adce8514315e72a3d05b
Ian Pan <ianpan@hotmail.com>
2017-04-22 11:48:54 -0700

    sum odd fib numbers first working solution

--- sumoddfib.js
+++ sumoddfib.js
@@ -1,5 +1,21 @@
 function sumFibs(num) {
-  return num;
+  // create fib sequence in an array, with the sequence ending one number after the num
+  var fibSequence = [1,1];
+  while (fibSequence[fibSequence.length -1] < num) {
+    fibSequence.push(fibSequence[fibSequence.length - 2] + fibSequence[fibSequence.length - 1]);
+  }
+  // then pop off the last element in array if it not equal to num
+  if (fibSequence[fibSequence.length -1] !== num) {
+    fibSequence.pop();
+  }
+  //add all the odd numbers in the sequence, return total 
+  var totalOdds = 0;
+  for (i = 0; i < fibSequence.length; i++) {
+    if ( fibSequence[i] % 2 !== 0) {
+      totalOdds += fibSequence[i];
+    }
+  }
+  return totalOdds;
 }
 
-sumFibs(4);
+sumFibs(75025);
